ab33317aa0242eb710e03845351979d05a965a6c
Alejandro Albarran <115290483+AAlejandro8@users.noreply.github.com>
2025-02-26 14:48:15 -0500

    added a html template to visualize the json data in a table format

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect, render_template
 import requests
 
 app = Flask(__name__)
@@ -7,32 +7,40 @@ app = Flask(__name__)
 
 
 # your local host
-EXTERNAL_API_URL = 'xxxx'
+EXTERNAL_API_URL = 'http://127.0.0.1:7000'
 
 
 def call_external_api(endpoint, auth=('user', 'pass')):
     try:
         response = requests.get(endpoint,auth=auth)
         if response.status_code == 200:
-            return jsonify(response.json())
+            return response.json()
         else:
-            return jsonify({"error": "Failed to fetch data", "status_code": response.status_code}),500
+            return {"error": "Failed to fetch data", "status_code": response.status_code}
     except requests.exceptions.RequestException as e:
-        return jsonify({"error": f"Failed to connect to the API: {str(e)}"}), 500
+        return {"error": f"Failed to connect to the API: {str(e)}"}
 
+
+# home page route
+@app.route('/home')
+@app.route('/')
+def home():
+     return render_template('base.html')
+     
 # Azure route
-@app.route('/azure/<data>', methods=['GET'])
-
+@app.route('/azure/<data>', methods=['GET','POST'])
 def call_azure(data):
         endpoint = (f'{EXTERNAL_API_URL}/oil/azure/{data}')
-        return call_external_api(endpoint)
+        response = call_external_api(endpoint)
+        return render_template('displayjson.html', data = response)
 
 # okta logs
 @app.route('/okta/<data>', methods=['GET'])
 
 def call_okta(data):
         endpoint = (f'{EXTERNAL_API_URL}/oil/okta/{data}')
-        return call_external_api(endpoint)
+        response = call_external_api(endpoint)
+        return render_template('displayjson.html', data = response)
                 
        
 if __name__ == '__main__':
